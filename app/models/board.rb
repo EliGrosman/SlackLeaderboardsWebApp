accf349d76611f8802d5640556f2045aad70b845
EliGrosman <eligrosman1@gmail.com>
2019-11-03 01:12:34 -0700

    Add has many/belongs to relation to Boards and Points

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,6 +1,8 @@
 class Board < ApplicationRecord
   has_many :match
   has_many :tournament_match
+  has_many :point
+  
   validates :board_name, presence: true
 
   before_destroy {
--- app/models/point.rb
+++ app/models/point.rb
@@ -1,2 +1,3 @@
 class Point < ApplicationRecord
+  belongs_to :board
 end
