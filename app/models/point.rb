d958bff832ea6007cbe627c61c14bbd159a8c094
Eli Grosman <eligrosman1@gmail.com>
2019-11-06 13:51:27 -0800

    Create unique code on points create

--- app/models/point.rb
+++ app/models/point.rb
@@ -1,3 +1,12 @@
 class Point < ApplicationRecord
   belongs_to :board
+
+  before_create :create_unique_identifier
+
+  def create_unique_identifier
+    loop do
+      self.code = SecureRandom.hex(5) 
+      break unless self.class.exists?(:code => code)
+    end
+  end
 end
