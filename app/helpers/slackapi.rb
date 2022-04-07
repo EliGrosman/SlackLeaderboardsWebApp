ed428fe1a2a59acb8a44432efdbc30a4b01263db
Eli Grosman <eligrosman1@gmail.com>
2022-04-06 18:34:06 -0700

    final? fix for slackApi issue

--- app/helpers/slackapi.rb
+++ app/helpers/slackapi.rb
@@ -1,34 +1,38 @@
 class Slackapi
 
   def self.getRealName(userid)
-    url = URI.parse("https://slack.com/api/users.info")
-    params = {"user" => userid}
-    headers = {"Authorization" => "Bearer #{ENV["SLACK_ACCESS_TOKEN"]}"}
+    uri = URI.parse("https://slack.com/api/users.info?user=#{userid}")
 
-    http = Net::HTTP.new(url.host, url.port)
-    response = http.post(url.path, params.to_json, headers)
-    puts(response)
-    if JSON.load(response.body)["error"]
+    res = Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
+      req = Net::HTTP::Get.new(uri)
+      req['Authorization'] = "Bearer #{ENV["SLACK_ACCESS_TOKEN"]}"
+
+      http.request(req)
+    end
+
+    if JSON.load(res.body)["error"]
       return nil
     else
-      profile = JSON.load(response.body)["user"]["profile"]
+      profile = JSON.load(res.body)["user"]["profile"]
       return profile["first_name"] + " " + (profile["last_name"][0]||" ").upcase
     end
   end
 
   def self.getAllUsers()
     url = URI.parse("https://slack.com/api/users.list")
-    params = {"token" => ENV["SLACK_ACCESS_TOKEN"]}
-    headers = {"Authorization" => "Bearer #{ENV["SLACK_ACCESS_TOKEN"]}"}
 
-    http = Net::HTTP.new(url.host, url.port)
-    response = http.post(url.path, params.to_json, headers)
-    if JSON.load(response.body)["error"]
+    res = Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
+      req = Net::HTTP::Get.new(uri)
+      req['Authorization'] = "Bearer #{ENV["SLACK_ACCESS_TOKEN"]}"
+
+      http.request(req)
+    end
+    if JSON.load(res.body)["error"]
       return nil
     else
       users = []
       ids = []
-      JSON.load(response.body)["members"].each do |user|
+      JSON.load(res.body)["members"].each do |user|
         if(!user["is_bot"] && user["real_name"] != "Slackbot")
           users << user["real_name"]
           ids << user["id"]
